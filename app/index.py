a5dd07c53c96268c8fc9a156e36b38e141b0038e
navneet-lang <navneet@weboappdiscovery.com>
2026-09-10 18:26:28 +0530

    lang

--- app/index.py
+++ app/index.py
@@ -6,7 +6,7 @@ from langchain_qdrant import QdrantVectorStore
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 load_dotenv()
-pdf_path = Path(__file__).parent / "data" / "sample.pdf"
+pdf_path = Path(__file__).parent / "data" / "COMPUTER NETWORKS NOTES.pdf"
 
 # PDF Loader
 loader = PyPDFLoader(str(pdf_path))
@@ -30,10 +30,10 @@ embedding_model =  OpenAIEmbeddings(
 
 #create  a Qdrant vectore 
 vector_store =  QdrantVectorStore(
-    documents = chunks,
+    # documents = chunks,
     embedding=embedding_model,
     collection_name="sample_collection",
-    url="http://localhost:6333"
+    # url="http://localhost:6333"
 )
 
 print("Vector store create and document embedded successfully. ")
